58a8a7f89de256f17261622bef1d65f6ccd80272
Brandon Hinds <brandon.hinds@hotmail.com>
2026-08-05 21:22:18 +1000

    Stamp Work billing compatibility fix

--- backend/version.js
+++ backend/version.js
@@ -2,7 +2,7 @@
 // Generated automatically by scripts/write-version.sh
 var BUILD_META = {
   buildDate: '2026-08-05',
-  buildCommit: '2fd35433a280'
+  buildCommit: '9f098d8a8c53'
 };
 function api_getBuildMeta() {
   return BUILD_META;
