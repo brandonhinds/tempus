0b173807b772391fa352fffca2cb5953cfc1e29e
Brandon Hinds <brandon.hinds@hotmail.com>
2026-08-08 09:51:34 +1000

    Stamp contextual time entry update

--- backend/version.js
+++ backend/version.js
@@ -1,8 +1,8 @@
 // AUTO-GENERATED FILE. DO NOT EDIT MANUALLY.
 // Generated automatically by scripts/write-version.sh
 var BUILD_META = {
-  buildDate: '2026-08-05',
-  buildCommit: '9f098d8a8c53'
+  buildDate: '2026-08-07',
+  buildCommit: '85e31224c558'
 };
 function api_getBuildMeta() {
   return BUILD_META;
