306564a37ebeb1bb32160cc9f41e89b9255945e2
Brandon Hinds <brandon.hinds@hotmail.com>
2026-08-08 20:36:35 +1000

    Stamp entry sync reliability update

--- backend/version.js
+++ backend/version.js
@@ -2,7 +2,7 @@
 // Generated automatically by scripts/write-version.sh
 var BUILD_META = {
   buildDate: '2026-08-08',
-  buildCommit: '8d05fa431e5c'
+  buildCommit: '28930f1bc1a2'
 };
 function api_getBuildMeta() {
   return BUILD_META;
